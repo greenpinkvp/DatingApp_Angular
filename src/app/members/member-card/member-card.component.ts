f044dd429c022e266b351e79b8380c1e9d568275
hongntb <bichhongbxvp@gmail.com>
2023-03-13 14:32:28 +0700

    add like feature + pagination

--- src/app/members/member-card/member-card.component.ts
+++ src/app/members/member-card/member-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { Member } from 'src/app/_model/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-card',
@@ -9,4 +11,15 @@ import { Member } from 'src/app/_model/member';
 })
 export class MemberCardComponent {
   @Input() member: Member | undefined;
+
+  constructor(
+    private memberService: MembersService,
+    private toastr: ToastrService
+  ) {}
+
+  addLike(member: Member) {
+    this.memberService.addLike(member.username).subscribe({
+      next: () => this.toastr.success('You have liked ' + member.knownAs),
+    });
+  }
 }
